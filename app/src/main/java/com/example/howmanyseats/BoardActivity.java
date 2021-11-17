dc2d637e9f9815a1510eff5ce54110d24675f9fb
Choi-Ju12g <choi-ju12g@naver.com>
2021-11-17 13:57:17 +0900

    firt test/board.v1

--- app/src/main/java/com/example/howmanyseats/BoardActivity.java
+++ app/src/main/java/com/example/howmanyseats/BoardActivity.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class BoardActivity extends AppCompatActivity {
 
     @Override
@@ -16,10 +18,22 @@ public class BoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_board);
 
         ListView listView = findViewById(R.id.lv_userComment);
+
+        MyAdapter adapter = new MyAdapter();
+        adapter.addItem(new userComment("최정우","1999.06.19","I hate this program",3));
+        adapter.addItem(new userComment("이기태","1999.02.23","I want wide room",1));
+        adapter.addItem(new userComment("이상윤","1999.05.21","fuck DB",5));
+        adapter.addItem(new userComment("고정훈","2000.02.23","why this is wrong",2));
+        listView.setAdapter(adapter);
+
     }
 
-    class myAdapter extends BaseAdapter{
+    class MyAdapter extends BaseAdapter{
+        private ArrayList<userComment> comments = new ArrayList<>();
 
+        public void addItem(userComment comment){
+            comments.add(comment);
+        }
         @Override
         public int getCount() {
             return 0;
