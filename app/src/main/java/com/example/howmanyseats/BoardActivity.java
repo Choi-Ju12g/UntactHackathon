d31bc73801d0a20ddf065d3eb2f1c5f5b810993f
Choi-Ju12g <65705571+Choi-Ju12g@users.noreply.github.com>
2021-11-19 06:17:50 +0900

    fuck conflict in misc.xml (#9)
    * storage store layout load using glide
    
    * draw store's layout dynamical
    
    * pull conlflict solve

--- app/src/main/java/com/example/howmanyseats/BoardActivity.java
+++ app/src/main/java/com/example/howmanyseats/BoardActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,10 +21,12 @@ import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.howmanyseats.Geocoding.GeoPointer;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -42,6 +48,8 @@ public class BoardActivity extends AppCompatActivity {
     Intent intent;
     FirebaseAuth auth;
     FirebaseFirestore db;
+    String[] thing_list;
+    int thing_cnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +70,13 @@ public class BoardActivity extends AppCompatActivity {
         introduce = findViewById(R.id.introduce);
         introduce.setText(intent.getStringExtra("introduce"));
         totalSeats = findViewById(R.id.totalSeat);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         totalSeats.setText("남은 자리 " + intent.getStringExtra("totalSeat"));
         name = findViewById(R.id.name);
@@ -72,6 +87,7 @@ public class BoardActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         //db에서 사용자 이름 찾기
         DocumentReference docRef = db.collection("member").document(auth.getCurrentUser().getEmail().toString());
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -90,6 +106,68 @@ public class BoardActivity extends AppCompatActivity {
             }
         });
 
+        // 배치도 그리는 부분
+        thing_list = intent.getStringArrayExtra("positionIndex");
+        thing_cnt = thing_list.length;
+        String[] r = new String[4];
+        ArrayList<Thing> thingArrayList = new ArrayList<>();
+        //Log.d("ssibal","test"+Integer.toString(thing_cnt));
+        for(int v = 0; v<thing_cnt;v++){
+            r=customParser(thing_list[v]);
+            //Log.d("ssibal","test"+v+":"+r[0]+"/"+r[1]+"/"+r[2]+"/"+r[3]);
+            thingArrayList.add(new Thing(r));
+        }
+
+        /*
+        for(int v=0; v<thingArrayList.size();v++){
+            Log.d("ssibal","cont"+thingArrayList.get(v).getX()+" "+thingArrayList.get(0).getY()+" "+thingArrayList.get(0).type);
+        }*/
+
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics ();
+        display.getMetrics(outMetrics);
+
+        float density = getResources().getDisplayMetrics().density;
+        float dpHeight = outMetrics.heightPixels / density;
+        float dpWidth = outMetrics.widthPixels / density;
+        System.out.println("dpWidth = " + dpWidth);
+        float rate = dpWidth/1440;
+        //System.out.println("raterate = " + rate);
+
+        for(int v=0; v<thingArrayList.size();v++){
+            thingArrayList.get(v).set_ratio_xy(rate);
+            Log.d("ssibal","cont"+thingArrayList.get(v).getX()+" "+thingArrayList.get(v).getY()+" "+thingArrayList.get(v).type);
+        }
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.uc_relativeCanvas);
+        for(int v=0; v<thingArrayList.size();v++){
+            ImageView v1 = new ImageView(getBaseContext());
+
+            String filename = thingArrayList.get(v).getType();
+            if(filename.equals("twotable24")){
+                v1.setImageResource(R.drawable.twotable24);
+            }else if(filename.equals("twotable_using24")){
+                v1.setImageResource(R.drawable.twotable_using24);
+            }else if(filename.equals("fourtable_using24")){
+                v1.setImageResource(R.drawable.fourtable_using24);
+            }else if(filename.equals("fourtable24")){
+                v1.setImageResource(R.drawable.fourtable24);
+            }else if(filename.equals("counter24")){
+                v1.setImageResource(R.drawable.counter24);
+            }else if(filename.equals("door24")){
+                v1.setImageResource(R.drawable.door24);
+            }else if(filename.equals("toilets24")){
+                v1.setImageResource(R.drawable.toilets24);
+            }else if(filename.equals("kiosk24")){
+                v1.setImageResource(R.drawable.kiosk24);
+            }
+
+            v1.setBackgroundColor(00000000);
+            v1.setPadding(thingArrayList.get(v).getX(),thingArrayList.get(v).getY(),0,0);
+            relativeLayout.addView(v1);
+        }
+
+
         //
         // list view 화면 띄우기 addapter 이용
         this.initData();
@@ -106,27 +184,29 @@ public class BoardActivity extends AppCompatActivity {
             }
         });
 
-        store_layout = (ImageView) findViewById(R.id.uc_store_layout);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageReference = storage.getReference();
-        StorageReference pathReference = storageReference.child("layout");
-        if(pathReference == null){
-            Toast.makeText(BoardActivity.this, "저장소에 사진이 없습니다",Toast.LENGTH_SHORT).show();
 
-        }else{
-            StorageReference submitProfile = storageReference.child("layout/storelayout.jfif");
-            submitProfile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    Glide.with(BoardActivity.this).load(uri).into(store_layout);
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-
-                }
-            });
-        }
+        //이미지 불러와서 설계도 올리기
+//        store_layout = (ImageView) findViewById(R.id.uc_store_layout);
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageReference = storage.getReference();
+//        StorageReference pathReference = storageReference.child("layout");
+//        if(pathReference == null){
+//            Toast.makeText(BoardActivity.this, "저장소에 사진이 없습니다",Toast.LENGTH_SHORT).show();
+//
+//        }else{
+//            StorageReference submitProfile = storageReference.child("layout/storelayout.jfif");
+//            submitProfile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                @Override
+//                public void onSuccess(Uri uri) {
+//                    Glide.with(BoardActivity.this).load(uri).into(store_layout);
+//                }
+//            }).addOnFailureListener(new OnFailureListener() {
+//                @Override
+//                public void onFailure(@NonNull Exception e) {
+//
+//                }
+//            });
+//        }
     }
 
     public void initData(){
@@ -144,6 +224,15 @@ public class BoardActivity extends AppCompatActivity {
         commentDataList.add(new userComment("이상윤","1999.05.21","fuck DB",5));
         commentDataList.add(new userComment("고정훈","2000.02.23","why this is wrong",2));
     }
+
+    // a 는 fourtable,x좌표,y좌표,사용여부
+    public String[] customParser(@NonNull String a){
+        String[] info = new String[4];
+        info = a.split(",");
+        return info;
+    }
+
+
 }
 
 class MyAdapter extends BaseAdapter{
